fbb31485deaa0d6a778e3cf22fc9c53e93130a7b
Victor <victor.viniciusggs@gmail.com>
2021-06-28 04:34:04 -0300

    'Dia dos namorados

---
+++ DDN.js
@@ -0,0 +1,166 @@
+var a=window.document.getElementById('click1'),b=document.getElementById('click2'),c=document.getElementById('click3'),d=document.getElementById('click4'), e=document.getElementById('click5'),f=document.getElementById('click6'),g=document.getElementById('click7'),h=window.document.getElementById('click8'),i=document.getElementById('click9'),j=document.getElementById('click10'),k=document.getElementById('click11'), l=document.getElementById('click12'),m=document.getElementById('click13'),n=document.getElementById('click14'),o=window.document.getElementById('click15'),p=document.getElementById('click16'),q=document.getElementById('click17'),r=document.getElementById('click18'), s=document.getElementById('click19'),t=document.getElementById('click20'),u=document.getElementById('click21')
+
+function clicou1(){
+    
+    a.innerHTML='Obrigado por esses dois anos!!'
+    a.style.background='yellow'
+    }
+    function saiu1(){
+       
+        
+        a.style.background='yellow'
+       
+    }
+function clicou2(){
+   
+     b.innerText='I'
+     b.style.background='yellow'
+ }
+ function saiu(){
+       
+    
+    b.style.background='yellow'
+   
+}
+ function clicou3(){
+   
+     c.innerText='LOVE'
+     c.style.background='yellow'
+ }
+ function saiu3(){
+       
+   
+    c.style.background='yellow'
+}
+
+ function clicou4(){
+    d.innerText='Cada momento com você é incrivel!!'
+    d.style.background='yellow'
+ }
+ function saiu4(){
+       
+   
+    d.style.background='yellow'
+}
+
+function clicou5(){
+    e.innerText='YOU'
+    e.style.background='yellow'
+ }
+ function saiu5(){
+    e.style.background='yellow'
+}
+
+function clicou6(){
+    f.innerText='HONEY'
+    f.style.background='yellow'
+ }
+ function saiu6(){
+    f.style.background='yellow'
+}
+function clicou7(){
+   g.innerText='❤'
+    g.style.background='yellow'
+ }
+ function saiu7(){
+    g.style.background='yellow'
+}
+function clicou8(){
+    h.innerText='HAPPY'
+    h.style.background='yellow'
+ }
+ function saiu8(){
+    h.style.background='yellow'
+}
+function clicou9(){
+    i.innerText='VALENTINES'
+    i.style.background='yellow'
+ }
+ function saiu9(){
+    i.style.background='yellow'
+}
+function clicou10(){
+    j.innerText='DAY'
+    j.style.background='yellow'
+ }
+ function saiu10(){
+    j.style.background='yellow'
+}
+function clicou11(){
+    k.innerText='❤'
+    k.style.background='yellow'
+ }
+ function saiu11(){
+    k.style.background='yellow'
+}
+function clicou12(){
+    l.innerText='CAN DREAM'
+    l.style.background='yellow'
+ }
+ function saiu12(){
+    l.style.background='yellow'
+}
+function clicou13(){
+    m.innerText='IF YOU'
+    m.style.background='yellow'
+ }
+ function saiu13(){
+    m.style.background='yellow'
+}
+function clicou14(){
+    n.innerText='YOU CAN'
+    n.style.background='yellow'
+ }
+ function saiu14(){
+    n.style.background='yellow'
+}
+function clicou15(){
+    o.innerText='DO IT'
+    o.style.background='yellow'
+ }
+ function saiu15(){
+    o.style.background='yellow'
+}
+function clicou16(){
+    p.innerText='QUE OS PROBLEMAS'
+    p.style.background='yellow'
+ }
+ function saiu16(){
+    p.style.background='yellow'
+}
+function clicou17(){
+    q.innerText='NUNCA NOS ATRAPALHEM'
+    q.style.background='yellow'
+ }
+ function saiu17(){
+    q.style.background='yellow'
+}
+function clicou18(){
+    r.innerText='E QUE NOSSO '
+    r.style.background='yellow'
+ }
+ function saiu18(){
+    r.style.background='yellow'
+}
+function clicou19(){
+    s.innerText='AMOR SEMPRE RECAREGUE '
+    s.style.background='yellow'
+ }
+ function saiu19(){
+    s.style.background='yellow'
+}
+function clicou20(){
+    t.innerText='OS NOSSOS'
+    t.style.background='yellow'
+ }
+ function saiu20(){
+    t.style.background='yellow'
+}
+function clicou21(){
+    u.innerText='CORAÇÕES'
+    u.style.background='yellow'
+ }
+ function saiu21(){
+    a.style.background='Crimson',b.style.background='Crimson',c.style.background='Crimson',d.style.background='Crimson',e.style.background='Crimson',f.style.background='Crimson',g.style.background='Crimson',h.style.background='Crimson',i.style.background='Crimson',j.style.background='Crimson',k.style.background='Crimson',l.style.background='Crimson',m.style.background='Crimson',n.style.background='Crimson',o.style.background='Crimson',p.style.background='Crimson',q.style.background='Crimson',r.style.background='Crimson',s.style.background='Crimson',t.style.background='Crimson',u.style.background='Crimson',
+    a.innerText='❤',b.innerText='❤',c.innerText='❤',d.innerText='❤',e.innerText='❤',f.innerText='❤',g.innerText='❤',h.innerText='❤',i.innerText='❤',j.innerText='❤',k.innerText='❤',l.innerText='❤',m.innerText='❤',n.innerText='❤',
+    o.innerText='❤',p.innerText='❤',q.innerText='❤',r.innerText='❤',s.innerText='❤',t.innerText='❤',u.innerText='❤'}
